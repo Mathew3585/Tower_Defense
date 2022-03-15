9dce89203f86883747ea35b08d21762bd47306a3
Mathew3585 <68810417+Mathew3585@users.noreply.github.com>
2022-03-15 12:56:32 +0100

    update

--- Assets/Script/WaveSpawner.cs
+++ Assets/Script/WaveSpawner.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
-public class WaveSpawner : MonoBehaviour
+public class WaveSpawner: MonoBehaviour
 {
     //Fonctions
     [SerializeField]
