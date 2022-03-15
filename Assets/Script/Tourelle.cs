9f1ff6f593edab5c92fe60317bf6896f2c0871bd
Mathew3585 <68810417+Mathew3585@users.noreply.github.com>
2022-03-15 20:25:06 +0100

    Rotation tourelle

--- Assets/Script/Tourelle.cs
+++ Assets/Script/Tourelle.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Tourelle : MonoBehaviour
 {
     //Fonctions
-    public Transform target;
+    private Transform target;
     public float range = 15f;
     public string enemyTag = "Enemy";
 
+    public Transform partToRotate;
+
+    public float turnSpeed = 10f;
     //Lancer en boucle la fonctions Updatetarget
     void Start()
     {
@@ -46,10 +49,23 @@ public class Tourelle : MonoBehaviour
     }
 
 
-    // Update is called once per frame
+    // Faire La rotaion de La tourelle
     void Update()
     {
-        
+        if(target == null)
+        {
+            return;
+        }
+
+        //Calucule la position de l'ennemis moin notre positions
+        Vector3 dir = target.position - transform.position;
+
+        //Faire la rotation
+        Quaternion LookRotation = Quaternion.LookRotation(dir);
+        //Appliquer la rotation
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, LookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
     }
 
 
