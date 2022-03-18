c64eaae49b815e71175ef396bd1183911df43168
Mathew3585 <68810417+Mathew3585@users.noreply.github.com>
2022-03-18 14:51:35 +0100

    update

--- Assets/Script/Player_Stat.cs
+++ Assets/Script/Player_Stat.cs
@@ -7,10 +7,14 @@ public class Player_Stat : MonoBehaviour
     //Foncition
     public static int money;
     public int startmoney = 400;
+    public static int lives;
+    public int startLife = 20;
 
     public void Start()
     {
         money = startmoney;
+        lives = startLife;
+
     }
 
 }
