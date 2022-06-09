cf4fdb82538af023146d09af00e49b9d4b7b7520
Mathew3585 <mathew.simon2004@gmail.com>
2022-06-09 22:26:12 +0200

    Ui mise en place

---
+++ Assets/Script/Menu/Menu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+
+    }
+}
